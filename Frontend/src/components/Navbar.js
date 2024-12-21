7324a60712bfd789a9065ddf18a2b25c6f73379a
Moassar Rizwi <imermoassarrizwi@gmail.com>
2024-12-21 10:53:27 +0530

    This Is Collaboration Project

---
+++ Frontend/src/components/Navbar.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './Navbar.css';
+
+const Navbar = () => {
+    const navigate = useNavigate();
+    const userString = localStorage.getItem('user');
+    const user = userString?JSON.parse(userString):null;
+    console.log(user)
+
+    const handleLogout = () => {
+        // Remove user data from local storage
+        localStorage.removeItem('user');
+        // Redirect to landing page
+        navigate('/');
+    };
+
+    //hlo
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img width='45' className='ms-2' src='https://res.cloudinary.com/dknnsbar3/image/upload/v1734712529/digital-collaboration_kqgui7.png'/>
+            </a>
+                <Link className="navbar-brand" to="/">CollabTool</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav me-auto">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/dashboard">Dashboard</Link>
+                        </li>
+                    </ul>
+                    {user ? (
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <button className="btn btn-link nav-link" onClick={handleLogout}>{user.username}  Logout</button>
+                            </li>
+                        </ul>
+                    ) : (
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/login">Login</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/register">Register</Link>
+                            </li>
+                        </ul>
+                    )}
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
